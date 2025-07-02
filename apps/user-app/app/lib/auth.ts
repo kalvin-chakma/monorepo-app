058af7f6d4cd71e5a5a179d3d6c92f6c54866f94
Kalvin Chakma <123386698+kalvin-chakma@users.noreply.github.com>
2025-07-02 23:05:43 +0600

    Added user auth

---
+++ apps/user-app/app/lib/auth.ts
@@ -0,0 +1,57 @@
+import { client } from "@repo/db/client";
+import CredentialProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import type { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+
+        if (!credentials) return null;
+
+        const existingUser = await client.user.findUnique({
+          where: { username: credentials.username }, 
+        });
+
+        if (existingUser) {
+          const passwordMatch = await bcrypt.compare(
+            credentials.password,
+            existingUser.password
+          );
+          if (passwordMatch) {
+            return {
+              id: existingUser.id.toString(),
+              username: existingUser.username,
+            };
+          }
+          return null;
+        }
+
+        try {
+          const hashPassword = await bcrypt.hash(credentials.password, 10);
+
+          const user = await client.user.create({
+            data: {
+              username: credentials.username,
+              password: hashPassword,
+            },
+          });
+
+          return {
+            id: user.id.toString(),
+            username: user.username,
+          };
+        } catch (error) {
+          console.error("Error creating user:", error);
+          return null; 
+        }
+      },
+    }),
+  ],
+};
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -3,7 +3,7 @@ import { client } from "@repo/db/client";
 export default async function Home() {
   const user = await client.user.findFirst();
   return (
-    <div>
+    <div className="text-3xl font-bold underline">
       kalvin chakma
       {user?.username}
       {user?.password}
