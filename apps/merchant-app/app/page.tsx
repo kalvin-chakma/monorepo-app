7b531cd6473783ae08fbdaf01212e1e7b9f996eb
Kalvin Chakma <123386698+kalvin-chakma@users.noreply.github.com>
2025-07-04 23:21:57 +0600

    setup tailwind css for packages/ui folder

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -1,9 +1,11 @@
 import { client } from "@repo/db/client";
+import { Appbar } from "@workspace/ui/components/appbar";
 
 export default async function Home() {
   const user = await client.user.findFirst();
   return (
     <div className="text-3xl font-bold underline">
+      <Appbar />
       kalvin chakma
       {user?.username}
       {user?.password}
